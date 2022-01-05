f227821f2065b8ea014e932af1e2e82df4d85dee
jose@luis <jose@luis>
2022-01-04 20:27:07 -0600

    Subiendo Proyecto

---
+++ app.js
@@ -0,0 +1,55 @@
+let pagina = 1;
+const btnAnterior = document.getElementById('btnAnterior');
+const btnSiguiente = document.getElementById('btnSiguiente');
+
+btnSiguiente.addEventListener('click', () => {
+	if(pagina < 1000){
+		pagina += 1;
+		cargarPeliculas();
+	}
+});
+
+btnAnterior.addEventListener('click', () => {
+	if(pagina > 1){
+		pagina -= 1;
+		cargarPeliculas();
+	}
+});
+
+const cargarPeliculas = async() => {
+	try {
+		const respuesta = await fetch(`https://api.themoviedb.org/3/movie/popular?api_key=d0ad63e935bdd18b987e6d8c7dfaf9a2&language=es-MX&page=${pagina}`);//HACEMOS LA PETICIÓN
+	
+		console.log(respuesta);
+
+		// Si la respuesta es correcta
+		if(respuesta.status === 200){
+			const datos = await respuesta.json(); //Se ocupa await porque es asyncrono
+			
+			let peliculas = '';
+			datos.results.forEach(pelicula => {
+				peliculas += `
+					<div class="pelicula">
+						<img class="poster" src="https://image.tmdb.org/t/p/w500/${pelicula.poster_path}">
+						<h3 class="titulo">${pelicula.title}</h3>
+					</div>
+				`;
+			});
+
+			document.getElementById('contenedor').innerHTML = peliculas;
+
+		} else if(respuesta.status === 401){
+			console.log('Pusiste la llave mal');
+		} else if(respuesta.status === 404){
+			console.log('La pelicula que buscas no existe');
+		} else {
+			console.log('Hubo un error y no sabemos que paso');
+		}
+
+	} catch(error){
+		console.log(error);
+	}
+
+}
+
+cargarPeliculas();
